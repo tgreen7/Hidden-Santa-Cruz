7b141f3b160164b7695a2d355883973fdb203fcd
tgreen7 <greenmachine777@gmail.com>
2015-12-04 17:26:33 -0800

    fixed review styling and avg truncates

--- applications/HiddenSC/models/tables.py
+++ applications/HiddenSC/models/tables.py
@@ -58,9 +58,9 @@ db.define_table('posts',
 
 db.define_table('reviews',
                 Field('body','text'),
-                Field('user_id',db.auth_user,default=auth.user_id),
+                Field('user_id',db.auth_user,default=auth.user_id, writable=False, readable=False),
                 Field('num_stars', 'integer'),
-                Field('post',db.posts)
+                Field('post',db.posts, writable=False, readable=False)
                 )
 db.reviews.num_stars.requires=IS_INT_IN_RANGE(0,6)
 
